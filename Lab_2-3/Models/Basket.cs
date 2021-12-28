19f8cfa272e3a8b474749c3e52f5448c7f545c49
ryabovdaniil <72159173+ryabovdaniil@users.noreply.github.com>
2021-12-29 00:00:36 +0300

    Add files via upload

---
+++ Lab_2-3/Models/Basket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Farmacy.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+    }
+}
