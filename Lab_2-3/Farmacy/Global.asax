64a69e7222eec2c5fd08838e4a0747c8e989b4a8
ryabovdaniil <riabov.daniil.uren@yandex.ru>
2021-12-29 20:59:10 +0300

    Web-lab
    Web-lab

---
+++ Lab_2-3/Farmacy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Farmacy.MvcApplication" Language="C#" %>
---
+++ Lab_2-3/Farmacy/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Farmacy.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+      
+        public string Person { get; set; }
+
+        public string Address { get; set; }
+
+        public int ItemId { get; set; }
+  
+    }
+}
